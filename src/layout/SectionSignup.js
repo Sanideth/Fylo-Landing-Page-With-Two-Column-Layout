9c77386d557e4e03e1284621cea68c901dd0a5ab
Sandro Bochorishvili <Sanideth@gmail.com>
2021-04-26 00:03:28 +0400

    added sectionSignup layout component and done markup for the corresponding section

---
+++ src/layout/SectionSignup.js
@@ -0,0 +1,37 @@
+import Heading from "../components/Heading";
+import Paragraph from "../components/Paragraph";
+import Input from "../components/Input";
+import Button from "../components/Button";
+
+const SectionSignup = () => {
+  return (
+    <section className="section-signup">
+      <div className="section-signup__text-container">
+        <Heading
+          element="h3"
+          headingText="Get early access today"
+          headingClassName="tertiary"
+        />
+        <Paragraph
+          paragraphText="It only takes a minute to sign up and our free starter tier is extremely generous. 
+  If you have any questions, our support team would be happy to help you."
+          paragraphClassName="tertiary"
+        />
+      </div>
+      <form action="#" className="section-signup__form">
+        <div className="section-signup__form-container">
+          <Input
+            type="email"
+            name="email"
+            ariaLabel="email"
+            className="section-signup"
+            placeholder="email@example.com"
+          />
+          <Button type="form" text="Get Started For Free" className="primary" />
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default SectionSignup;
