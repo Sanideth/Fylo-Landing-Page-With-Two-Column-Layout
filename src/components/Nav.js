dc2000c61da27587a1cc1c661fc87da94392ad51
Sandro Bochorishvili <Sanideth@gmail.com>
2021-04-26 19:41:37 +0400

    added ul to the list, which I forgot :D

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,15 @@
 const Nav = (props) => {
   return (
     <nav className={`${props.className}__nav`}>
-      {props.arr.map((item, index) => (
-        <li className={`${props.className}__item`} key={index}>
-          <a href={props.href} className={`${props.className}__link`}>
-            {item}
-          </a>
-        </li>
-      ))}
+      <ul className={`${props.className}__list`}>
+        {props.arr.map((item, index) => (
+          <li className={`${props.className}__item`} key={index}>
+            <a href={props.href} className={`${props.className}__link`}>
+              {item}
+            </a>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 };
