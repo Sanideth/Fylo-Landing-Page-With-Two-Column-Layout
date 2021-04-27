2de7d134705913c54940574e21c4a037e9e71d41
Sandro Bochorishvili <Sanideth@gmail.com>
2021-04-27 21:51:04 +0400

    finished styling of section signup for desktop

--- src/layout/SectionSignup.js
+++ src/layout/SectionSignup.js
@@ -6,30 +6,38 @@ import Button from "../components/Button";
 const SectionSignup = () => {
   return (
     <section className="section-signup">
-      <div className="section-signup__text-container">
-        <Heading
-          element="h3"
-          headingText="Get early access today"
-          headingClassName="tertiary"
-        />
-        <Paragraph
-          paragraphText="It only takes a minute to sign up and our free starter tier is extremely generous. 
+      <div className="container">
+        <div className="section-signup__content">
+          <div className="section-signup__text-container">
+            <Heading
+              element="h3"
+              headingText="Get early access today"
+              headingClassName="tertiary"
+            />
+            <Paragraph
+              paragraphText="It only takes a minute to sign up and our free starter tier is extremely generous. 
   If you have any questions, our support team would be happy to help you."
-          paragraphClassName="tertiary"
-        />
-      </div>
-      <form action="#" className="section-signup__form">
-        <div className="section-signup__form-container">
-          <Input
-            type="email"
-            name="email"
-            ariaLabel="email"
-            className="section-signup"
-            placeholder="email@example.com"
-          />
-          <Button type="form" text="Get Started For Free" className="primary" />
+              paragraphClassName="tertiary"
+            />
+          </div>
+          <form action="#" className="section-signup__form">
+            <div className="section-signup__form-container">
+              <Input
+                type="email"
+                name="email"
+                ariaLabel="email"
+                className="section-signup"
+                placeholder="email@example.com"
+              />
+              <Button
+                type="form"
+                text="Get Started For Free"
+                className="primary"
+              />
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
     </section>
   );
 };
