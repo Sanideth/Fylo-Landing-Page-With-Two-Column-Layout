339e498050df10314163fece4152289b418fefa3
Sandro Bochorishvili <Sanideth@gmail.com>
2021-04-29 22:26:53 +0400

    added onchange handler and some props from form state

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,19 @@
 const Input = (props) => {
+  const addErrorClass = props.error ? "error" : "";
+
+  const handleOnChange = (e) => {
+    props.handleOnChange(e.target.value);
+  };
+
   return (
     <input
       type={props.type}
-      className={`${props.className}__input`}
+      className={`${props.className}__input ${addErrorClass}`}
       aria-label={props.ariaLabel}
       placeholder={props.placeholder}
       name={props.name}
+      value={props.text}
+      onChange={handleOnChange}
     />
   );
 };
